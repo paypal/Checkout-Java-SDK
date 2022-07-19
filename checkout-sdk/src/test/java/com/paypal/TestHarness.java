fc333266ca9beace70e2b157aafc8ccf1bff66f9
Timothy Martinak <timothy.martinak@getbraintree.com>
2022-07-19 16:33:53 +0000

    Bump paypalhttp-java dep
    * Add additional files to .gitignore
    * Update test harness to respect README env vars for test runs

--- checkout-sdk/src/test/java/com/paypal/TestHarness.java
+++ checkout-sdk/src/test/java/com/paypal/TestHarness.java
@@ -7,22 +7,22 @@ import org.testng.annotations.BeforeMethod;
 
 public class TestHarness {
 
-    private HttpClient client;
-    private PayPalEnvironment environment;
+  private HttpClient client;
+  private PayPalEnvironment environment;
 
-    @BeforeMethod
-    public void setup() {
-        environment = new PayPalEnvironment.Sandbox(
-                "AdV4d6nLHabWLyemrw4BKdO9LjcnioNIOgoz7vD611ObbDUL0kJQfzrdhXEBwnH8QmV-7XZjvjRWn0kg",
-                "EPKoPC_haZMTq5uM9WXuzoxUVdgzVqHyD5avCyVC1NCIUJeVaNNUZMnzduYIqrdw-carG9LBAizFGMyK");
-        client = new PayPalHttpClient(environment);
-    }
+  @BeforeMethod
+  public void setup() {
+    environment = new PayPalEnvironment.Sandbox(
+      System.getenv("PAYPAL_CLIENT_ID"),
+      System.getenv("PAYPAL_CLIENT_SECRET"));
+    client = new PayPalHttpClient(environment);
+  }
 
-    protected HttpClient client() {
-        return client;
-    }
+  protected HttpClient client() {
+    return client;
+  }
 
-    protected PayPalEnvironment environment() {
-        return environment;
-    }
+  protected PayPalEnvironment environment() {
+    return environment;
+  }
 }
